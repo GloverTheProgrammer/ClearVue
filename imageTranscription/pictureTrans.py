f4acde8d4605b9af595a101dfd11f3b3a0ac7036
David <davidjrglover@gmail.com>
2024-03-09 15:06:29 -0500

    imageTranscription

---
+++ imageTranscription/pictureTrans.py
@@ -0,0 +1,52 @@
+import os
+import base64
+from openai import OpenAI
+import requests
+
+
+api_key="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"
+
+image_path = "/Users/redap/Desktop/IMG_4350.png"
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    return encoded_string.decode('utf-8')
+
+base64_image = encode_image(image_path)
+
+def classify(base64_image, api_key):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+
+    payload = {
+    "model":"gpt-4-vision-preview",
+    "messages":[
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": "Provide a comprehensive description of the image, without mentioning its a photograph or scene to the user, for a visually impaired person, focusing on identifying key objects, characters, and any text, including their arrangement and interactions within the scene. Describe the setting, atmosphere, and highlight any notable emotional or thematic elements. Include details on colors, shapes, and textures to enrich the description. If present, accurately transcribe text within the image. This description should help a visually impaired individual visualize the content and context as if they were seeing it themselves, all within a concise limit of three sentences",
+            },
+            {
+            "type": "image_url",
+            "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+            },
+            },
+        ],
+        }
+    ],
+    "max_tokens" : "300",
+    }
+
+    try:
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        print(response.json())
+    except Exception as e:
+        print(e)
+
+classify(base64_image, api_key)
