230a310ac01bd4d27a62a7b7b73533da5a48cfb7
LukeFenstermacher <luke.fenstermacher@gmail.com>
2024-03-09 17:37:46 -0500

    Button Hold Change Mode
    Added logic to change the "mode" of the program based on the hold of the button.

--- imageTranscription/button_press.py
+++ imageTranscription/button_press.py
@@ -4,22 +4,39 @@ import RPi.GPIO as GPIO
 
 
 def button_press():
+
     BUTTON_GPIO = 16
     DELAY = 500
-    last_ms = 0
+    HOLD = 2200
+
+    start_ms = 0
+    start_press_ms = 0
+
+    mode = 0
+    
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     pressed = False
+    held = False
 
     while True:
-        # button is pressed when pin is LOW
+
         if not GPIO.input(BUTTON_GPIO):
-            if not pressed and (time.time() * 1000 - last_ms > DELAY):
-                print("Button pressed!")
+            if not pressed and (time.time() * 1000 - start_ms > DELAY):
                 pressed = True
-                last_ms = time.time() * 1000
-        # button not pressed (or released)
+                start_ms = time.time() * 1000
+            if pressed and not held and (time.time() * 1000 - start_ms > HOLD):
+                held = True
+                if mode == 2:
+                    mode = 0
+                else
+                    mode = mode + 1
+                print(mode)
+
         else:
+            if pressed and not held:
+                print("pressed")
             pressed = False
+            held = False
         time.sleep(0.1)
