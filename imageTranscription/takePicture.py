d820324d6854cbfff1d2c321d249378ed3a623a1
David <davidjrglover@gmail.com>
2024-03-09 16:09:23 -0500

    Update takePicture.py

--- imageTranscription/takePicture.py
+++ imageTranscription/takePicture.py
@@ -1,18 +1,17 @@
 import cv2
 import os
 
-
-def save_image():
+def save_image(directory="/home/blackhatDesktop/transcribe/"):
+    # Create the directory if it doesn't exist
+    
     cam = cv2.VideoCapture(0)
-
     cv2.namedWindow("test")
-
     img_counter = 0
 
     while True:
         ret, frame = cam.read()
         if not ret:
-            print("failed to grab frame")
+            print("Failed to grab frame")
             break
         cv2.imshow("test", frame)
 
@@ -23,13 +22,12 @@ def save_image():
             break
         elif k%256 == 32:
             # SPACE pressed
-            img_name = "opencv_frame_{}.png".format(img_counter)
+            img_name = os.path.join(directory, f"opencv_frame_{img_counter}.png")
             cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
+            print(f"{img_name} written!")
             img_counter += 1
 
     cam.release()
-
     cv2.destroyAllWindows()
 
 save_image()
