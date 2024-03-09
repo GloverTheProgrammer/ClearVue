fecbceb2ff409f7cedbcceacf5c81a08c7a50e02
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-03-09 14:16:17 -0500

    removed CAP_V4L2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ torch.backends.quantized.engine = 'qnnpack'
 net = models.quantization.mobilenet_v2(pretrained=True, quantize=True)
 net = torch.jit.script(net)
 
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
 cap.set(cv2.CAP_PROP_FPS, 36)
