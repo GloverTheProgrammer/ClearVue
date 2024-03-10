d37e9d54c1c0d0c29e5568cd47f8a2f2089becba
erins98 <aaronsaijan@gmail.com>
2024-03-10 00:31:20 -0500

    changes to formating of AboutUs

--- website/src/templates/Base.tsx
+++ website/src/templates/Base.tsx
@@ -1,10 +1,10 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
+import { AboutUs } from './AboutUs';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
-import { AboutUs } from './AboutUs';
 
 const Base = () => (
   <div className="text-gray-600 antialiased">
