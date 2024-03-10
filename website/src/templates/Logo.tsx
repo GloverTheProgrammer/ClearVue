89cf039453cf6a72fd70ef4b012bc929e2450be6
erins98 <aaronsaijan@gmail.com>
2024-03-09 19:41:55 -0500

    added logo

--- website/src/templates/Logo.tsx
+++ website/src/templates/Logo.tsx
@@ -12,24 +12,7 @@ const Logo = (props: ILogoProps) => {
 
   return (
     <span className={`inline-flex items-center text-gray-900 ${fontStyle}`}>
-      <svg
-        className="mr-1 stroke-current text-primary-500"
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M0 0h24v24H0z" stroke="none" />
-        <rect x="3" y="12" width="6" height="8" rx="1" />
-        <rect x="9" y="8" width="6" height="12" rx="1" />
-        <rect x="15" y="4" width="6" height="16" rx="1" />
-        <path d="M4 20h14" />
-      </svg>
-
+      <img src="assets/images/logo.svg" alt="Logo" />
       {AppConfig.site_name}
     </span>
   );
