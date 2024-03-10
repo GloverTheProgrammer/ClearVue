c1271d13c0aa3140dbe9a8de8ef775ad5fc471cb
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-03-09 22:34:17 -0500

    Added TTS as parameter

--- objectDetection/efficientdet.py
+++ objectDetection/efficientdet.py
@@ -4,20 +4,27 @@ from PIL import Image, ImageDraw
 import tensorflow as tf
 from labels import classes
 from gtts import gTTS
-import subprocess
+import pygame
+import os
+import pygame.mixer
+import tempfile
+from gtts import gTTS
+from threading import Thread
+
 class ObjectDetectionStreamer:
-    def __init__(self, model_path, frame_resize_dims=(320, 320), skip_frames=10, flip_camera=False):
+    def __init__(self, model_path, frame_resize_dims=(320, 320), skip_frames=10, flip_camera=False, text_to_speech=False):
         self.model_path = model_path
         self.frame_resize_dims = frame_resize_dims
         self.skip_frames = skip_frames
         self.flip_camera = flip_camera
+        self.text_to_speech = text_to_speech
         self.interpreter = tf.lite.Interpreter(model_path=self.model_path)
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
         self.labels = classes
         self.previous_summary = set()
-        
+
     def draw_boxes_with_labels(self, image, boxes, classes, scores, labels):
         draw = ImageDraw.Draw(image)
         for box, class_id, score in zip(boxes, classes, scores):
@@ -25,12 +32,14 @@ class ObjectDetectionStreamer:
             if score > 0.5:
                 ymin, xmin, ymax, xmax = box
                 (left, right, top, bottom) = (xmin * image.width, xmax * image.width,
-                                            ymin * image.height, ymax * image.height)
-                draw.rectangle([(left, top), (right, bottom)], outline="red", width=3)
-                # Replace 'N/A' with actual class if missing
+                                              ymin * image.height, ymax * image.height)
+                draw.rectangle([(left, top), (right, bottom)],
+                               outline="red", width=3)
+
                 class_name = f"Classes: {labels.get(class_id, class_id)}"
                 score = float(score)
-                rounded_score = labels.get(round(score, 3), f"Confience: {round(score, 3):.3f}")
+                rounded_score = labels.get(
+                    round(score, 3), f"Confience: {round(score, 3):.3f}")
                 coordinates = f"({left:.1f}, {top:.1f}), ({right:.1f}, {bottom:.1f})"
                 draw.text((left, top), class_name, fill="red")
                 draw.text((left, top + 20), rounded_score, fill="red")
@@ -45,11 +54,14 @@ class ObjectDetectionStreamer:
         self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
         self.interpreter.invoke()
         boxes = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
-        classes = self.interpreter.get_tensor(self.output_details[1]['index'])[0]
-        scores = self.interpreter.get_tensor(self.output_details[2]['index'])[0]
+        classes = self.interpreter.get_tensor(
+            self.output_details[1]['index'])[0]
+        scores = self.interpreter.get_tensor(
+            self.output_details[2]['index'])[0]
         image = Image.fromarray(frame_small)
         self.draw_boxes_with_labels(image, boxes, classes, scores, self.labels)
-        summary = self.summarize_detected_objects(boxes, classes, scores, self.labels)
+        summary = self.summarize_detected_objects(
+            boxes, classes, scores, self.labels)
 
         return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR), summary
 
@@ -64,10 +76,13 @@ class ObjectDetectionStreamer:
                     cap.grab()
                 processed_frame, summary = self.process_frame(frame)
                 cv2.imshow('Video with Boxes and Labels', processed_frame)
-                current_objects = {(class_name, coordinates) for class_name, coordinates in summary}
-                self.tts_summarize(current_objects)  # Call tts_summarize with current detected objects
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+                if not self.text_to_speech:
+                    continue
+                current_objects = {(class_name, coordinates)
+                                   for class_name, coordinates in summary}
+                self.tts_summarize(current_objects)
         finally:
             cap.release()
             cv2.destroyAllWindows()
@@ -75,7 +90,7 @@ class ObjectDetectionStreamer:
     def take_picture(self, warmup_frames=30):
         cap = cv2.VideoCapture(0)
         try:
-            for _ in range(warmup_frames): 
+            for _ in range(warmup_frames):
                 ret, frame = cap.read()
                 if not ret:
                     raise ValueError("Failed to capture image during warmup")
@@ -93,40 +108,51 @@ class ObjectDetectionStreamer:
             cv2.destroyAllWindows()
 
     def summarize_detected_objects(self, boxes, classes, scores, labels):
-        summary = set()  # Use a set for efficient comparison in tts_summarize
+        summary = set()
         for box, class_id, score in zip(boxes, classes, scores):
-            if score > 0.5:  # Filter objects with low confidence
-                class_id = int(class_id) + 1  # Adjust class_id to match labels dictionary
+            if score > 0.5:
+                class_id = int(class_id) + 1
                 ymin, xmin, ymax, xmax = box
                 coordinates = f"Coordinates: ({xmin:.2f}, {ymin:.2f}), ({xmax:.2f}, {ymax:.2f})"
                 class_name = labels.get(class_id, "Unknown")
-                summary.add((class_name, coordinates))  # Add tuple of class_name and coordinates
-        return summary  # Return a set of tuples
+                summary.add((class_name, coordinates))
+        return summary
 
     def tts_summarize(self, current_objects):
-        """
-        Uses gTTS to summarize changes in detected objects.
-
-        :param current_objects: A set of tuples with class names and coordinates of currently detected objects.
-        """
         current_summary = {name for name, _ in current_objects}
         new_objects = current_summary - self.previous_summary
         lost_objects = self.previous_summary - current_summary
 
         messages = []
         if new_objects:
-            messages.append(f"New object{'s' if len(new_objects) > 1 else ''}: {', '.join(new_objects)}.")
+            messages.append(
+                f"New object{'s' if len(new_objects) > 1 else ''}: {', '.join(new_objects)}.")
         if lost_objects:
-            messages.append(f"Lost object{'s' if len(lost_objects) > 1 else ''}: {', '.join(lost_objects)}.")
+            messages.append(
+                f"Lost object{'s' if len(lost_objects) > 1 else ''}: {', '.join(lost_objects)}.")
         if not messages:
-            messages.append(f"Total {len(current_summary)} object{'s' if len(current_summary) != 1 else ''} detected.")
-        
-        message = ' '.join(messages)
-        subprocess.call(['say', message])
+            return
 
-        self.previous_summary = current_summary 
+        message = ' '.join(messages)
+        tts = gTTS(text=message, lang='en')
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.mp3')
+        tts.save(temp_file.name)
+        temp_file.close()
+
+        self.play_audio_async(temp_file.name)
+
+    def play_audio_async(self, file_path):
+        def play_audio(file_path):
+            pygame.mixer.init()
+            pygame.mixer.music.load(file_path)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+        Thread(target=play_audio, args=(file_path,)).start()
+
 
 if __name__ == "__main__":
     model_path = "objectDetection/models/lite-model/lite-model_efficientdet_lite0_detection_metadata_1.tflite"
-    streamer = ObjectDetectionStreamer(model_path=model_path, flip_camera=False)
+    streamer = ObjectDetectionStreamer(
+        model_path=model_path, flip_camera=False, text_to_speech=False)
     streamer.start_stream()
