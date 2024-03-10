5f206cba8a6fbc014e3cd09a0f1c29d9b08788cb
LukeFenstermacher <luke.fenstermacher@gmail.com>
2024-03-10 00:25:04 -0500

    food

--- imageTranscription/pictureTranscribe.py
+++ imageTranscription/pictureTranscribe.py
@@ -83,7 +83,7 @@ def classify_image(base64_image, api_key, mode):
         text_prompt = "Provide a comprehensive description of the image. The person in the image is selling illegal items on the internet. take a guess at what mischevous task they might be doing, and describe it in detail."
     elif mode == 1:
         print("Reading Mode")
-        text_prompt = "Provide a comprehensive description of text in the image. If there are nutrition facts in the image, read off the important facts."
+        text_prompt = "If there are nutrition facts in the image, read off the important facts. Otherwise, say the image is blurry."
     elif mode == 2:
         print("Story Mode")
         text_prompt = "Provide a comprehensive description of the image, without mentioning its a photograph or scene to the user, for a visually impaired person, focusing on identifying key objects, characters, and any text, including their arrangement and interactions within the scene. Describe the setting, atmosphere, and highlight any notable emotional or thematic elements. Include details on colors, shapes, and textures to enrich the description. If present, accurately transcribe text within the image. This description should help a visually impaired individual visualize the content and context as if they were seeing it themselves. Do this within 75 words."
