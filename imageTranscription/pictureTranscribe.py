a80ab25f76a89962bfbe2c3d6e87366f0c15fedc
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-03-10 01:58:39 -0500

    Update pictureTranscribe.py

--- imageTranscription/pictureTranscribe.py
+++ imageTranscription/pictureTranscribe.py
@@ -156,6 +156,7 @@ def main():
                     ObjectDetectionStreamer.ObjectDetectionStreamer.main()
                 finally:
                     system_ready = True  # Ensure system_ready is reset even if exited
+                    mode = base_mode % 4  # Reset mode to base mode
             else:
                 base_mode = mode
                 system_ready = False  # Prevent further actions
